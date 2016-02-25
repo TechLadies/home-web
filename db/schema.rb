e8d6738afb20fdbb73b98a954c347879ba9cde5f
nettocornetto <swong496@gmail.com>
2016-02-25 23:13:59 +0800

    create model with columns for user

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160224145139) do
-
-  create_table "organizations", force: :cascade do |t|
-    t.string   "name"
-    t.string   "phone"
-    t.text     "address"
-    t.string   "industry"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
