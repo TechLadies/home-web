5b944a8b3b19676a77871a4d4d807aa9c06debec
[sandytseng] <[sandytseng0828@gmail.com]>
2016-03-01 16:05:41 +0000

    case has_many issues

--- app/models/case.rb
+++ app/models/case.rb
@@ -1,3 +1,4 @@
 class Case < ActiveRecord::Base
+  has_many :issues
   validates :user_id, :organization_id, presence: true
 end
--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,3 +1,4 @@
 class Issue < ActiveRecord::Base
+	belongs_to :case
 	validates :case, presence: true
 end
