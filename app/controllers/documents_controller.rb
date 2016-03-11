a9897ab03d69b51d4447aa9e955fd99e0e8f110f
katelyp <katelimyinpin@gmail.com>
2016-03-11 23:42:12 +0800

    create documents model and controller

---
+++ app/controllers/documents_controller.rb
@@ -0,0 +1,24 @@
+class DocumentsController < ApplicationController
+
+  def new
+    @document = Document.new
+  end
+
+  def create
+    @document = Document.new(document_params)
+    if @document.save
+      redirect_to @document
+      flash
+#?how to redirect to the case associated with the new document?#
+    else
+      render 'new'
+    end
+  end
+
+private
+
+  def document_params
+    params.require(:document).permit(:case_id, :file_url)
+  end
+
+end
