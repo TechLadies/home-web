66351864aff3e0c994492efa8d645a70de15643d
katelyp <katelimyinpin@gmail.com>
2016-03-11 21:47:40 +0800

    completed case controller

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -1,8 +1,10 @@
 class CasesController < ApplicationController
 
-#work in progress#  
   def close
-  	:status
+  	@case = Case.find(params[:id])
+    @case.update(status: "Closed")
+    flash[:success] = "Case Closed!"
+    redirect_to @case
   end
 
   def index
@@ -10,10 +12,17 @@ class CasesController < ApplicationController
   end
 
   def new
-  	@case = Case.all
+  	@case = Case.new
   end
 
   def create
+  	@case = Case.new(case_params)
+  	if @case.save
+      flash[:success] = "New Case Saved!"
+  	  redirect_to @case
+  	else
+  	  render 'new'
+  	end
   end
 
   def show
@@ -25,6 +34,19 @@ class CasesController < ApplicationController
   end
 
   def update
+  	@case = Case.find(params[:id])
+    if @case.update_attributes(case_params)
+      flash[:success] = "Case Updated!"
+      redirect_to @case      	
+    else
+      render 'edit'
+    end
   end
+
+  private
+
+    def case_params
+      params.require(:case).permit(:user_id, :organization_id, :case_type, :status)
+    end   
 
 end
