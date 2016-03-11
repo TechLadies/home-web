a0de3d066f2bae39e175a3e4ff357d6bf10e181d
katelyp <katelimyinpin@gmail.com>
2016-03-11 22:35:51 +0800

    edit actions controller

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -1,2 +1,23 @@
 class ActionsController < ApplicationController
+
+  def new
+    @action = Action.new
+  end
+
+  def create
+    @action = Action.new(action_params)
+    if @action.save
+      redirect_to @action
+#?how to redirect to the case associated with the new action?#
+    else
+      render 'new'
+    end
+  end
+
+private
+
+  def action_params
+    params.require(:action).permit(:case_id, :user_id, :description, :status)
+  end
+
 end
