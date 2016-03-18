2071240e3fb302a6c598f2fc3bc0e01c90565cfb
katelyp <katelimyinpin@gmail.com>
2016-03-18 15:21:56 +0800

    migration: remove_status_from_actions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160312094020) do
+ActiveRecord::Schema.define(version: 20160318071734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 20160312094020) do
     t.integer  "case_id"
     t.integer  "user_id"
     t.text     "description"
-    t.string   "status"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
