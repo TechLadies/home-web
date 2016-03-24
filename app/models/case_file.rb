3c35a107ec6a6de03cc498d0940971aa91833dc2
katelyp <katelimyinpin@gmail.com>
2016-03-24 13:20:41 +0800

    if issue-description is blank, save with original msg

--- app/models/case_file.rb
+++ app/models/case_file.rb
@@ -8,7 +8,7 @@ class CaseFile < ActiveRecord::Base
   has_many :documents, foreign_key: :case_id, inverse_of: :case_file
   has_many :people, through: :involvements
 
-  accepts_nested_attributes_for :issues, reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :issues, reject_if: proc { |a| a['description'].blank? }, allow_destroy: true
 
   validates :user, :case_type, :status, presence: true
 
