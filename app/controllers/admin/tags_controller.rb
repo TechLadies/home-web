6929e9ab39ccb95eb457c4b60067a43df69ceda0
katelyp <katelimyinpin@gmail.com>
2016-03-27 20:20:17 +0800

    view for tag#index

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -3,3 +3,5 @@ class Admin::TagsController < ApplicationController
   	@tags = Tag.all
   end
 end
+
+#     resources :tags, only: [:index, :new, :create, :edit, :update]
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,8 +1,6 @@
 class Tag < ActiveRecord::Base
 
-  belongs_to :parent_tag, class_name: Tag
-  has_many :children_tags, class_name: Tag, foreign_key: :parent_tag_id
-
-  has_many: issues
+  belongs_to :parent_tag, class_name: :Tag
+  has_many :children_tags, class_name: :Tag, foreign_key: :parent_tag_id
 
 end
