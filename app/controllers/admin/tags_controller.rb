a1ee6b35c0d88ed241ae96964f6ce17b89a4be77
katelyp <katelimyinpin@gmail.com>
2016-03-29 17:14:04 +0800

    failed attemp to build view for Tag#new

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -1,7 +1,47 @@
 class Admin::TagsController < ApplicationController
+
+  before_action :prepare_tag, only: [:edit, :update, :parent]
+
   def index
   	@tags = Tag.all
   end
-end
 
-#     resources :tags, only: [:index, :new, :create, :edit, :update]
+  def new
+  	@tag = Tag.new
+  end
+
+  def create
+  	@tag = Tag.new(tag_params)
+  	if @tag.save
+  	  redirect_to tags_path
+#  	  render [:admin, @tag]
+  	  flash[:notice] = 'New Tag Created'
+  	else
+  	  render :new
+      flash[:error] = 'Tag Not Created! Please Retry.'      
+  	end
+  end
+
+  def edit
+  end  
+
+  def update
+    if @tag.update_attributes(tag_params)
+      flash[:success] = 'Tag Updated!'
+      redirect_to tags_path       
+    else
+      render :edit
+      flash[:error] = 'Tag Not Updated! Please Retry.'
+    end
+  end
+
+private
+  def prepare_tag
+    @tag = Tag.find(params[:id])
+  end
+
+  def tag_params
+    params.require(:tag).permit(:name, :parent_tag_id, :description)
+  end
+
+end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,8 @@
 class Tag < ActiveRecord::Base
 
-  belongs_to :parent_tag, class_name: :Tag
-  has_many :children_tags, class_name: :Tag, foreign_key: :parent_tag_id
+  belongs_to :parent, class_name: :Tag
+  has_many :children, class_name: :Tag, foreign_key: :parent_tag_id
+
+  validates :name, :description, presence: true
 
 end
