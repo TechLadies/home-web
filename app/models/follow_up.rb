416141563a17399b1344c698db6da075f4dba3bb
nettocornetto <swong496@gmail.com>
2016-04-04 23:58:30 +0800

    relate paper trail gem in case file, followup and issue

--- app/models/follow_up.rb
+++ app/models/follow_up.rb
@@ -1,7 +1,8 @@
 class FollowUp < ActiveRecord::Base
 
-  belongs_to :case_file
+  belongs_to :case_file, foreign_key: :case_id, inverse_of: :follow_ups
   belongs_to :user
+  has_paper_trail
 
   validates :case_file, :user, :description, presence: true
 
--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,6 +1,7 @@
 class Issue < ActiveRecord::Base
 
   belongs_to :case_file, foreign_key: :case_id, inverse_of: :issues
+  has_paper_trail
 
   validates :case_file, presence: true
 
